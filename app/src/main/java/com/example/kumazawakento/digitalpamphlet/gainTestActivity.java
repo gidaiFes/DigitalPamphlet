f8d006ed18129743cda518d836ed2412a5c686a9
kumazawakento <kento.kumazawa@gmail.com>
2016-11-19 21:54:00 +0900

    addTestAct

---
+++ app/src/main/java/com/example/kumazawakento/digitalpamphlet/gainTestActivity.java
@@ -0,0 +1,13 @@
+package com.example.kumazawakento.digitalpamphlet;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class gainTestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gain_test);
+    }
+}
