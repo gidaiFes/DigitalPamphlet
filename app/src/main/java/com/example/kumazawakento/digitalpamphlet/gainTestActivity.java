fee1414d907d27a174a602ea41c78c856ac10856
kumazawakento <kento.kumazawa@gmail.com>
2016-11-20 20:03:19 +0900

    add gradle

--- app/src/main/java/com/example/kumazawakento/digitalpamphlet/gainTestActivity.java
+++ app/src/main/java/com/example/kumazawakento/digitalpamphlet/gainTestActivity.java
@@ -2,6 +2,7 @@ package com.example.kumazawakento.digitalpamphlet;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import  android.support.design.widget.TabLayout;
 
 public class gainTestActivity extends AppCompatActivity {
 
@@ -9,5 +10,14 @@ public class gainTestActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gain_test);
+
+
+        TabLayout tabLayout = (TabLayout)findViewById(R.id.tab_layout);
+        tabLayout.addTab(tabLayout.newTab().setText("タブ1"));
+        tabLayout.addTab(tabLayout.newTab().setText("タブ2"));
+        tabLayout.addTab(tabLayout.newTab().setText("タブ3"));
+
+
     }
+
 }
