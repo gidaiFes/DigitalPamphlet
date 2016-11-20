ca6edc79649b4ce8aba0d0d0fdea9535a1535c02
kumazawakento <kento.kumazawa@gmail.com>
2016-11-20 15:59:49 +0900

    addToolBar have err

--- app/src/main/java/com/example/kumazawakento/digitalpamphlet/kentTestActivity.java
+++ app/src/main/java/com/example/kumazawakento/digitalpamphlet/kentTestActivity.java
@@ -3,6 +3,7 @@ package com.example.kumazawakento.digitalpamphlet;
 import android.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.Window;
@@ -15,6 +16,13 @@ public class kentTestActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         getWindow().requestFeature(Window.FEATURE_ACTION_BAR);
         setContentView(R.layout.activity_kent_test);
+
+
+        // ツールバーをアクションバーとしてセット
+        Toolbar toolbar = (Toolbar) findViewById(R.id.tool_bar);
+        setSupportActionBar(toolbar);
+        toolbar.inflateMenu(R.menu.main);
+
     }
 
     @Override
@@ -39,6 +47,18 @@ public class kentTestActivity extends AppCompatActivity {
         Toast.makeText(this, "Selected Item: " + item.getTitle(), Toast.LENGTH_SHORT).show();
         return true;
     }
+
+    toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+        @Override
+        public boolean onMenuItemClick(MenuItem menuItem) {
+            int id = menuItem.getItemId();
+            if (id == R.id.action_settings) {
+                Toast.makeText(MainActivity.this, "settings clicked 2", Toast.LENGTH_SHORT).show();
+                return true;
+            }
+            return false;
+        }
+    });
 
 
 }
