8500d9e4292c7b19b0e3f34900d63c0dc37f4d1d
kumazawakento <kento.kumazawa@gmail.com>
2016-11-20 19:01:23 +0900

    test toolbar

--- app/src/main/java/com/example/kumazawakento/digitalpamphlet/kentTestActivity.java
+++ app/src/main/java/com/example/kumazawakento/digitalpamphlet/kentTestActivity.java
@@ -6,10 +6,13 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.view.Window;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class kentTestActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,25 +20,53 @@ public class kentTestActivity extends AppCompatActivity {
         getWindow().requestFeature(Window.FEATURE_ACTION_BAR);
         setContentView(R.layout.activity_kent_test);
 
+        final TextView textView = (TextView)findViewById(R.id.testTextView);
+
+
         // ツールバーをアクションバーとしてセット
         Toolbar toolbar = (Toolbar) findViewById(R.id.tool_bar);
         setSupportActionBar(toolbar);
+        toolbar.setNavigationIcon(R.mipmap.ic_launcher);
+
+        //toolbar.setLogo(R.drawable.gidai);
+        toolbar.setTitle("This is title");
+        toolbar.setSubtitle("This is sub-title");
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(kentTestActivity.this, "Navigation click", Toast.LENGTH_SHORT).show();
+                textView.setText("aa");
+            }
+        });
+
+
+
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+        /*
         // メニューの要素を追加
+        menu.add("Normal item");
+        menu.add("Normal item");
         menu.add("Normal item");
 
         // メニューの要素を追加して取得
         MenuItem actionItem = menu.add("Action Button");
+        MenuItem actionItem2 = menu.add("Action Button2");
+        MenuItem actionItem3 = menu.add("Action Button3");
 
         // SHOW_AS_ACTION_IF_ROOM:余裕があれば表示
         actionItem.setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM);
+        actionItem2.setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM);
+        actionItem3.setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM);
 
         // アイコンを設定
         actionItem.setIcon(android.R.drawable.ic_menu_share);
-
+        actionItem2.setIcon(android.R.drawable.ic_menu_share);
+        actionItem3.setIcon(android.R.drawable.ic_menu_share);
+*/
         return true;
     }
 
@@ -44,8 +75,17 @@ public class kentTestActivity extends AppCompatActivity {
         Toast.makeText(this, "Selected Item: " + item.getTitle(), Toast.LENGTH_SHORT).show();
         return true;
     }
+/*
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kent_test);
 
-
-
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.addTab(tabLayout.newTab().setText("tab 1"));
+        tabLayout.addTab(tabLayout.newTab().setText("tab 2"));
+        tabLayout.addTab(tabLayout.newTab().setText("tab 3"));
+    }
+*/
 
 }
