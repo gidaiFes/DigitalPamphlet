90846d3f6ada77dbe2e02b35fd81035f51081b53
裕登 <wagahai@outlook.com>
2016-11-20 13:18:35 +0900

    sendaiにtwitter接続機能作成

--- app/src/main/java/com/example/kumazawakento/digitalpamphlet/sendaiTestActivity.java
+++ app/src/main/java/com/example/kumazawakento/digitalpamphlet/sendaiTestActivity.java
@@ -2,12 +2,22 @@ package com.example.kumazawakento.digitalpamphlet;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;import android.annotation.SuppressLint;
 
 public class sendaiTestActivity extends AppCompatActivity {
-
+    @SuppressLint("SetJavaScriptEnabled")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sendai_test);
+        WebView twitterView = (WebView)findViewById(R.id.twitter_web);
+        //WebViewClient使用でアプリ内でtwitterを開くことができるのです（どやぁ）
+        twitterView.setWebViewClient(new WebViewClient());
+        twitterView.loadUrl("https://twitter.com/nut_fes");
+        //WebView内でのJavascript利用を有効にする
+        twitterView.getSettings().setJavaScriptEnabled(true);
     }
+
+
 }
