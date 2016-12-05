92197a95b3bb31c88c3b9d70ba2bcbeec816523b
kumazawakento <kento.kumazawa@gmail.com>
2016-12-06 00:47:12 +0900

    fixMain

--- app/src/main/java/com/example/kumazawakento/digitalpamphlet/ExampleFragment.java
+++ app/src/main/java/com/example/kumazawakento/digitalpamphlet/ExampleFragment.java
@@ -27,7 +27,7 @@ public class ExampleFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_main, null);
+        View view = inflater.inflate(R.layout.fragment_example, null);
         LinearLayout linearLayout = (LinearLayout) view.findViewById(R.id.fragment_main_linearlayout);
         linearLayout.setBackgroundResource(getArguments().getInt(BACKGROUND_COLOR));
 
--- app/src/main/java/com/example/kumazawakento/digitalpamphlet/MainActivity.java
+++ app/src/main/java/com/example/kumazawakento/digitalpamphlet/MainActivity.java
@@ -17,11 +17,12 @@ import com.viewpagerindicator.TitlePageIndicator;
 
 public class MainActivity extends AppCompatActivity {
 
-    private MainFragmentPagerAdapter mAdapter;
+    //private MainFragmentPagerAdapter mAdapter;
     private ViewPager mPager;
     private TitlePageIndicator mIndicator;
 
-
+//---------------------------------------------------------------------onCreate
+    //初期設定
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,45 +30,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        //-------------------------pager.tab.drawer
-        /*
-        Intent intent = new Intent(getApplication(), TabActivity.class);
-        startActivity(intent);
-        */
-
-        /*
-        //------------------------pager
-        mAdapter = new MainFragmentPagerAdapter(getSupportFragmentManager());
-
-        mPager = (ViewPager)findViewById(R.id.pager);
-        mPager.setAdapter(mAdapter);
-
-        TitlePageIndicator indicator = (TitlePageIndicator)findViewById(R.id.indicator);
-        mIndicator = indicator;
-        indicator.setViewPager(mPager);
-
-        final float density = getResources().getDisplayMetrics().density;
-        indicator.setBackgroundColor(0x18FF0000);
-        indicator.setFooterColor(0xFFAA2222);
-        indicator.setFooterLineHeight(1 * density); //1dp
-        indicator.setFooterIndicatorHeight(3 * density); //3dp
-        indicator.setFooterIndicatorStyle(TitlePageIndicator.IndicatorStyle.Underline);
-        indicator.setTextColor(0xAA000000);
-        indicator.setSelectedColor(0xFF000000);
-        indicator.setSelectedBold(true);
-        //-----------------------------------------------------------------------------
-        */
-
-
         //-----------------------ウィジェット紐付け
         Button gainButton = (Button)findViewById(R.id.button_gain);
         Button sendaiButton = (Button)findViewById(R.id.button_sendai);
         Button kentButton = (Button)findViewById(R.id.button_kent);
+        Button testButton = (Button)findViewById(R.id.button_test);
         //---------------------------------------
 
 
-
-        //------------- buttonListener gain
+        //------------- buttonListener
         gainButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
@@ -75,10 +46,6 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-        //------------------------------------
-
-
-        //------------- buttonListener sendai
         sendaiButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
@@ -86,13 +53,18 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-        //------------------------------------
-
-        //------------- buttonListener kent
         kentButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getApplication(), kentTestActivity.class);
+                startActivity(intent);
+            }
+        });
+        testButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                //-------------------------pager.tab.drawer
+                Intent intent = new Intent(getApplication(), TabActivity.class);
                 startActivity(intent);
             }
         });
@@ -102,32 +74,12 @@ public class MainActivity extends AppCompatActivity {
         //------------------------タブ設定
         TabLayout tabLayout = (TabLayout)findViewById(R.id.tab_layout);
         tabLayout.addTab(tabLayout.newTab().setText("HOME"));
-        tabLayout.addTab(tabLayout.newTab().setText("ARTIST"));
+        tabLayout.addTab(tabLayout.newTab().setText("MAP"));
         tabLayout.addTab(tabLayout.newTab().setText("TIME TABLE"));
         tabLayout.addTab(tabLayout.newTab().setText("FOODS"));
-        tabLayout.addTab(tabLayout.newTab().setText("GOODS"));
-
-
+        tabLayout.addTab(tabLayout.newTab().setText("EVENT"));
+        //------------------------------------
     }
 
-    public boolean onCreateOptionsMenu(Menu menu) {
 
-        menu.add("aaaaaaaa");
-
-        // メニューの要素を追加して取得
-        MenuItem actionItem = menu.add("Action Button Help Icon");
-        // アイコンを設定
-        actionItem.setIcon(android.R.drawable.ic_menu_help);
-
-        // SHOW_AS_ACTION_ALWAYS:常に表示
-        actionItem.setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
-
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        Toast.makeText(this, "Selected Item: " + item.getTitle(), Toast.LENGTH_SHORT).show();
-        return true;
-    }
 }
