cf183a1b89d8678c1ac02a5e96d57715c1445247
kumazawakento <kento.kumazawa@gmail.com>
2016-12-11 14:16:26 +0900

    Fixed

--- app/src/main/java/com/example/kumazawakento/digitalpamphlet/TabTestActivity.java
+++ app/src/main/java/com/example/kumazawakento/digitalpamphlet/TabTestActivity.java
@@ -1,10 +1,13 @@
 package com.example.kumazawakento.digitalpamphlet;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import  android.support.design.widget.TabLayout;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.widget.TextView;
 
 public class TabTestActivity extends AppCompatActivity {
@@ -14,6 +17,7 @@ public class TabTestActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tab_test);
+
 
         final TextView textView = (TextView)findViewById(R.id.testTextView);
 
@@ -26,12 +30,6 @@ public class TabTestActivity extends AppCompatActivity {
         toolbar.setTitle("This is title");
         toolbar.setSubtitle("Test Activity");
 
-        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish();
-            }
-        });
         //------------------------タブ設定
         TabLayout tabLayout = (TabLayout)findViewById(R.id.tab_layout);
         tabLayout.addTab(tabLayout.newTab().setText("HOME"));
