f464ebc3f71fb86dbd5420f81596cdab8cc87690
kumazawakento <kento.kumazawa@gmail.com>
2016-12-12 21:31:40 +0900

    Add toolbar to twitterAct

--- app/src/main/java/com/example/kumazawakento/digitalpamphlet/TwitterTestActivity.java
+++ app/src/main/java/com/example/kumazawakento/digitalpamphlet/TwitterTestActivity.java
@@ -2,6 +2,8 @@ package com.example.kumazawakento.digitalpamphlet;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;import android.annotation.SuppressLint;
 
@@ -11,6 +13,21 @@ public class TwitterTestActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_twitter_test);
+        // ツールバーをアクションバーとしてセット
+        Toolbar toolbar = (Toolbar) findViewById(R.id.tool_bar);
+        setSupportActionBar(toolbar);
+        toolbar.setNavigationIcon(R.mipmap.ic_launcher);
+        //toolbar.setLogo(R.drawable.gidai);
+        toolbar.setTitle("This is title");
+        toolbar.setSubtitle("Test Activity");
+
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
         WebView twitterView = (WebView)findViewById(R.id.twitter_web);
         //WebViewClient使用でアプリ内でtwitterを開くことができるのです（どやぁ）
         twitterView.setWebViewClient(new WebViewClient());
